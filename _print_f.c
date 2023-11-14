469b13e1ef44c020c132ffc15815d68cf2ddbaf0
Mohammed <mrcloud.med@gmail.com>
2023-11-14 21:46:48 +0100

    printf

--- _print_f.c
+++ _print_f.c
@@ -3,59 +3,44 @@
 /**
  * _printf - Function to print according to a format
  * @format: The First argument
- * 
+ *
  * Return: Count of how many charachter were printed
 */
 
 int _printf(const char *format, ...)
 {
-    int count, specifierIndex;
-    va_list args;
-    type diftype[] = {{'c', _print_char}, {'s', _print_str}};
-    count = 0;
+	int count = 0, specifierIndex;
+	va_list args;
+	type diftype[] = {{'c', _print_char}, {'s', _print_str}};
 
-    if (format == NULL)
-    {
-        return (-1);
-    }
-
-    va_start(args, format);
-
-    while (*format != '\0')
-    {
-        if (*format != '%')
-        {
-            count += _putchar(*format);
-        }
-        else
-        {
-            format++;
-            if (*format == '%')
-            {
-                count += _putchar('%');
-            }
-            else
-            {
-                specifierIndex = 0;
-                while (specifierIndex < 2)
-                {
-                    if (*format == diftype[specifierIndex].op)
-                    {
-                        count += diftype[specifierIndex].fun(args);
-                        break;
-                    }
-                    specifierIndex++;
-                }
-                if (specifierIndex == 2)
-                {
-                    va_end(args);
-                    return (-1);
-                }
-            }
-        }
-        format++;
-    }
-
-    va_end(args);
-    return (count);
+	if (format == NULL)
+		return (-1);
+	va_start(args, format);
+	while (*format != '\0')
+	{
+		format++;
+		if (*format != '%')
+			count += _putchar(*format);
+		else
+		{
+			if (*format == '%')
+				count += _putchar('%');
+			else
+			{
+				specifierIndex = 0;
+				while (specifierIndex < 2)
+				{
+					if (*format == diftype[specifierIndex].op)
+					{
+						count += diftype[specifierIndex].fun(args);
+						break;
+					}
+					specifierIndex++;
+				}
+			}
+		}
+		format++;
+	}
+	va_end(args);
+	return (count);
 }
